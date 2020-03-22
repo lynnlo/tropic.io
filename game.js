e599ff973520f16eddcfdece521ac11ef3039576
Saketh <51687055+AnAstutehuMan@users.noreply.github.com>
2020-03-22 14:54:58 -0700

    Formatted

--- game.js
+++ game.js
@@ -10,57 +10,59 @@ var slowmofactor = 1;
 var disableclear = false;
 
 function onload() {
-    canvas = document.getElementById("maincanvas");
-		screenx = screen.width
-		screeny = screen.height
-		canvasx = screen.width / 5
-		canvasy = screen.height / 5
-		scalex =  screenx / canvasx
-		scaley =  screeny / canvasy
-    canvas.width  = canvasx * scalex  
-    canvas.height = canvasy * scaley 
-    canvas.style.width = (canvasx * scalex  ).toString() + "px";
-    canvas.style.height = (canvasy * scaley ).toString() + "px";
-    context = canvas.getContext("2d");
-    addactor(10, 10, 10, 10, "#000000");
-		addactor(0,canvasy - (10),canvasx,10,"#000000");
-    ticker = setInterval(ontick, 10 * slowmofactor);
+	canvas = document.getElementById("maincanvas");
+	screenx = screen.width
+	screeny = screen.height
+	canvasx = screen.width / 5
+	canvasy = screen.height / 5
+	scalex = screenx / canvasx
+	scaley = screeny / canvasy
+	canvas.width = canvasx * scalex
+	canvas.height = canvasy * scaley
+	canvas.style.width = (canvasx * scalex).toString() + "px";
+	canvas.style.height = (canvasy * scaley).toString() + "px";
+	context = canvas.getContext("2d");
+	addactor(10, 10, 10, 10, "#000000");
+	addactor(0, canvasy - (10), canvasx, 10, "#000000");
+	ticker = setInterval(ontick, 10 * slowmofactor);
 }
 
 function addactor(posx, posy, width, height, color = "#ffffff", text = NaN) {
 	posx = posx * scalex
 	posy = posy * scaley
-	width  = width  * scalex
+	width = width * scalex
 	height = height * scaley
-    Objects.push({
-        posx,
-        posy,
-        width,
-        height,
-        color,
-        text
-    });
+	Objects.push({
+		posx,
+		posy,
+		width,
+		height,
+		color,
+		text
+	});
 }
 
 function ontick() {
-    if (disableclear == false) {
-        context.fillStyle = "#ffffff";
-        context.fillRect(0, 0, canvasx * scalex, canvasy * scaley);
-    }
-    for (i = 0; i < Objects.length; i++) {
-			crashed = false;
-			for (d = 0; d < Objects.length; d++) {
-				if (collide(Objects[i],Objects[d])) {crashed = true;}
+	if (disableclear == false) {
+		context.fillStyle = "#ffffff";
+		context.fillRect(0, 0, canvasx * scalex, canvasy * scaley);
+	}
+	for (i = 0; i < Objects.length; i++) {
+		crashed = false;
+		for (d = 0; d < Objects.length; d++) {
+			if (collide(Objects[i], Objects[d])) {
+				crashed = true;
 			}
-			if (Objects[i]["posy"] + Objects[i]["height"] <= canvas.height && crashed == false) {
-				Objects[i]["posy"] += 1 * scaley;
-			}
-			context.fillStyle = Objects[i]["color"];
-			context.fillRect(Objects[i]["posx"], Objects[i]["posy"], Objects[i]["width"], Objects[i]["height"]);
-    }
+		}
+		if (Objects[i]["posy"] + Objects[i]["height"] <= canvas.height && crashed == false) {
+			Objects[i]["posy"] += 1 * scaley;
+		}
+		context.fillStyle = Objects[i]["color"];
+		context.fillRect(Objects[i]["posx"], Objects[i]["posy"], Objects[i]["width"], Objects[i]["height"]);
+	}
 }
 
-function collide(a,b){
+function collide(a, b) {
 	at = a["posy"];
 	ab = a["posy"] + a["height"];
 	al = a["posx"];
